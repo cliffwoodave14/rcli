0392fcae949393c736a07db8b423166bfa62d063
cliffwoodave14 <cliffwoodave14@gmail.com>
2024-06-24 00:25:59 -0700

    add cli main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+
+/// rcli csv -i input.csv -o output.json --header -d ','
+
+use std::path::Path;
+
+use clap:: Parser;
+
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version, author, about)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Debug, Parser)]
+enum SubCommand {
+    #[command(name = "csv", about = "Convert CSV to JSON format")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long, help = "Input CSV file", default_value = "input.csv", value_parser = validate_input)]
+    input: String,
+
+    #[arg(short, long, help = "Output JSON file", default_value = "output.json")]  // "output.json".into()
+    output: String,
+
+    #[arg(short, long = "Delimiter", default_value_t = ',')]
+    delimiter: char,
+
+    #[arg(long, help = "CSV has header or not", default_value_t = true)]
+    header: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+    println!("{:?}", opts);
+}
+
+fn validate_input(input: &str) -> Result<String, &'static str> {
+    if !Path::new(input).exists() {
+        panic!("Input file does not exist");
+    } else {
+        Ok(input.into())
+    }
 }
